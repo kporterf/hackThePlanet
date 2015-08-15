17a903c3dfc8ea26cbd916379689c0a2dbe17cbd
Juan Alvarado <jalvarado91@gmail.com>
2015-08-15 15:25:33 -0700

    basic functionality and styles

--- app/src/main/java/moodify/com/moodify/MainActivity.java
+++ app/src/main/java/moodify/com/moodify/MainActivity.java
@@ -1,16 +1,104 @@
 package moodify.com.moodify;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerNotificationCallback;
+import com.spotify.sdk.android.player.PlayerState;
+import com.spotify.sdk.android.player.Spotify;
+import com.squareup.okhttp.Headers;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Playlist;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class MainActivity extends AppCompatActivity implements PlayerNotificationCallback, ConnectionStateCallback{
+
+    // TODO: Replace with your client ID
+    private static final String CLIENT_ID = "6106c54c402f4d878df000badf3384bf";
+    // TODO: Replace with your redirect URI
+    private static final String REDIRECT_URI = "moodify-auth://callback";
+
+    private Player mPlayer;
+    private String oauthToken;
+
+    private static final int REQUEST_CODE = 1337;
+
+    private static String playlists[] = { "spotify:user:jalvarado91:playlist:4dUVDjuZsVHPFkQwnyJfrk" };
+
+    public Button playButton;
+    public Button pauseButton;
+    public ImageView playlistArtImage;
+    public TextView moodLabel;
+    public TextView playlistLable;
+    public TextView ownerLable;
+
+    private SharedPreferences sharedPreferences;
+    private SpotifyApi spotifyApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPreferences = getSharedPreferences("com.moodify.PREFERENCE_FILES", MODE_PRIVATE);
+
+
+        playButton = (Button) findViewById(R.id.play_button);
+        pauseButton = (Button) findViewById(R.id.pause_button);
+        playlistArtImage = (ImageView) findViewById(R.id.playlist_art);
+        moodLabel = (TextView) findViewById(R.id.mood_label);
+        playlistLable = (TextView) findViewById(R.id.playlist_label);
+        ownerLable = (TextView) findViewById(R.id.owner_label);
+
+        spotifyApi = new SpotifyApi();
+
+        if(sharedPreferences.getString("token", null) == null || sharedPreferences.getString("token", "").isEmpty()) {
+            AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                    AuthenticationResponse.Type.TOKEN,
+                    REDIRECT_URI);
+            builder.setScopes(new String[]{"user-read-private", "streaming"});
+            AuthenticationRequest request = builder.build();
+            AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+        }
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    playNow();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
     }
 
     @Override
@@ -33,5 +121,132 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, data);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                oauthToken = response.getAccessToken();
+                sharedPreferences.edit().putString("token", oauthToken).commit();
+                spotifyApi.setAccessToken(sharedPreferences.getString("token", null));
+            }
+        }
+    }
+
+    public void playNow() throws Exception {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("http://192.168.3.163:3000/mood/chill")
+                .build();
+
+        client.newCall(request).enqueue(new com.squareup.okhttp.Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(com.squareup.okhttp.Response response) throws IOException {
+                if(!response.isSuccessful()) throw new IOException("Unexpected code");
+                String responseString = response.body().string();
+                try {
+                    JSONObject responseObject = new JSONObject(responseString);
+                    final String mood = responseObject.getString("mood");
+                    String rawPlaylistURL = responseObject.getString("playlistURL");
+
+                    String PlaylistURL = rawPlaylistURL.substring(
+                            0, rawPlaylistURL.indexOf("\u001b")
+                    );
+
+                    String[] tokens = PlaylistURL.split(":");
+                    String user = tokens[2];
+                    String playlist = tokens[4];
+
+                    SpotifyService spotify = spotifyApi.getService();
+
+                    spotify.getPlaylist(user, playlist, new Callback<Playlist>() {
+                        @Override
+                        public void success(Playlist playlist, Response response) {
+
+                            try {
+                                moodLabel.setText(mood.toUpperCase() + " MOOD");
+                                playlistLable.setText(playlist.name);
+                                if(playlist.owner.display_name == null || playlist.owner.display_name.isEmpty()) {
+                                    ownerLable.setVisibility(View.GONE);
+                                } else {
+                                    ownerLable.setText(playlist.owner.display_name);
+                                }
+                                Glide.with(MainActivity.this)
+                                        .load(playlist.images.get(0).url)
+                                        .into(playlistArtImage);
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+
+
+                        @Override
+                        public void failure(RetrofitError error) {
+
+                        }
+                    });
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+//                Log.v("BRO", mood);
+            }
+        });
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Throwable error) {
+        Log.d("MainActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
+    }
+
+    @Override
+    public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+        Log.d("MainActivity", "Playback event received: " + eventType.name());
+    }
+
+    @Override
+    public void onPlaybackError(ErrorType errorType, String errorDetails) {
+        Log.d("MainActivity", "Playback error received: " + errorType.name());
+    }
+
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
     }
 }
